aee94a0edc36ca58bd4ce6cca4a37f3ba0ff596d
Dan Stevens <dan.stevens@oracle.com>
2018-10-04 11:35:50 +0100

    Lesson 8.2

--- ReactJS Fundamentals/react-lesson-7/app/components/Profile.jsx
+++ ReactJS Fundamentals/react-lesson-7/app/components/Profile.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 
-let Profile = props => (
-  <div>
-    <h2>{props.name}</h2>
-    <p>
-      {props.name} is {props.age} and {props.bio}
-    </p>
-    <h3>Hobbies</h3>
-    <ul>
-      {props.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>)}
-    </ul>
-  </div>
-);
-export default Profile;
+// Profile component as a function instead of a class
+// let Profile = props => (
+//   <div>
+//     <h2>{props.name}</h2>
+//     <p>
+//       {props.name} is {props.age} and {props.bio}
+//     </p>
+//     <h3>Hobbies</h3>
+//     <ul>
+//       {props.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>)}
+//     </ul>
+//   </div>
+// );
+// export default Profile;
 
 // 'default' keyword allows caller to import this class without wrapping in braces. So instead of:
 //    import {Profile} from './components/Profile.jsx'
 // user can write:
 //    import Profile from './components/Profile.jsx'
-// export default class Profile extends React.Component {
-//   render() {
-//     let hobbies = this.props.hobbies.map(hobby => <li>{hobby}</li>);
-//     return (
-//       <div>
-//         <h1>{this.props.name}</h1>
-//         <p>
-//           {this.props.name} is {this.props.age} and {this.props.bio}
-//         </p>
-//         <h2>Hobbies</h2>
-//         <ul>
-//           {hobbies}
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
+export default class Profile extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    // Keep logic efficient and to a minimum - we are trying to improve performance by deciding
+    // whether or not to re-render the component. The test should cost less than the render cost to 
+    // be worthwhile
+    return this.props.name !== nextProps.name || this.props.age !== nextProps.age ||
+      this.props.bio !== nextProps.bio;
+  }
+
+  render() {
+    console.log('Rendering Profile component');
+    let hobbies = this.props.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>);
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>
+          {this.props.name} is {this.props.age} and {this.props.bio}
+        </p>
+        <h2>Hobbies</h2>
+        <ul>
+          {hobbies}
+        </ul>
+      </div>
+    )
+  }
+}
